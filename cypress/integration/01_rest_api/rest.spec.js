bba49433eace5f5f0323db8b6d24efed7e669f46
Gene Culling <culling66@gmail.com>
2022-03-27 21:38:49 +1300

    Add acceptance test for rest api

--- cypress/integration/01_rest_api/rest.spec.js
+++ cypress/integration/01_rest_api/rest.spec.js
@@ -1,11 +1,22 @@
 /// <reference types="cypress" />
 // import adminUser from '../../../cypress/fixtures/users/adminUser.json';
+describe("canary", () => {
+    it("should always be true", () => {
+        expect(true).to.equal(true);
+    })
+})
 
 describe('get from rest endpoint', () => {
-    it("should login to jira", () => {
-        const endpointUrl = Cypress.config().baseUrl + "/plugins/servlet/timestamper/api"
+    it("should get a valid response from timestamper/api/:data?", () => {
+        const endpointUrl = Cypress.config().baseUrl + "/plugins/servlet/timestamper/api/1451001600000"
         cy.request(endpointUrl).then(response => {
-            console.log(endpointUrl);
+            const body = response.body;
+            console.log("body: ", body);
+            expect(body.unix).to.equal(1451001600000);
+            // expect(body.utc).to.equal("Fri, 25 Dec 2015 00:00:00 GMT");
+            // expect(body).to.equal({
+            //     unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"
+            // });
         });
 
     });
